50d5bb0b2ebd1c6c928b25925dac7b36bc87a5fe
Alex Scordellis <alex.scordellis@gmail.com>
2010-01-12 08:50:05 +0000

    Trying to post to campfire - failing with 422.

---
+++ src/ccnet.campfire.plugin.tests.connected/PostMessages.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ccnet.campfire.plugin.tests.connected
+{
+    [TestFixture]
+    public class PostMessages
+    {
+        [Test]
+        public void can_post_a_message_then_retrieve_it()
+        {
+            var room = new CampfireRoom("ccnetcampfireplugin",
+                                        "4c7838c235627478c86d2c6b6ed60b512cdb8303",
+                                        265935);
+
+            room.Join();
+
+            room.Post("A message from your build server");
+
+            Assert.That(room.Transcript.Last(), Is.EqualTo("A message from your build server"));
+
+            room.Leave();
+        }
+    }
+}
---
+++ src/ccnet.campfire.plugin/CampfireRoom.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace ccnet.campfire.plugin
+{
+    public class CampfireRoom
+    {
+        private readonly string accountName;
+        private readonly string authToken;
+        private readonly int roomId;
+
+        public CampfireRoom(string accountName, string authToken, int roomId)
+        {
+            this.accountName = accountName;
+            this.authToken = authToken;
+            this.roomId = roomId;
+        }
+
+        public void Join()
+        {
+            PostTo("join").GetResponse();
+        }
+
+        private HttpWebRequest PostTo(string action)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(string.Format("http://{0}.campfirenow.com/room/{1}/{2}.xml" , accountName, roomId, action));
+            request.Method = "POST";
+            request.Headers[HttpRequestHeader.Authorization] = string.Format("Basic {0}", EncodedAuthToken);
+            return request;
+        }
+
+        private string EncodedAuthToken
+        {
+            get
+            {
+                byte[] bytesToEncode = Encoding.ASCII.GetBytes(authToken + ":X");
+                return Convert.ToBase64String(bytesToEncode);
+            }
+        }
+
+        public void Post(string message)
+        {
+            var request = PostTo("speak");
+            using (var sw = new StreamWriter(request.GetRequestStream()))
+            {
+                sw.Write("<message><type>TextMessage</type><body>" + message + "</body></message>");
+            }
+            request.GetResponse();
+        }
+
+        public IEnumerable<string> Transcript
+        {
+            get { yield break; }
+        }
+
+        public void Leave()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
